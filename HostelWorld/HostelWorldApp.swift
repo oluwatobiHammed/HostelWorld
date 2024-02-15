4ea2c5204a1b2b85f7d546f7d7d60c21406a72e2
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-02-15 19:17:38 +0000

    Initial Commit

---
+++ HostelWorld/HostelWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostelWorldApp.swift
+//  HostelWorld
+//
+//  Created by Oladipupo Oluwatobi on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HostelWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
