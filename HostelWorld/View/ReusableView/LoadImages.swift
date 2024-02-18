04e7593a42107de94c3d7c4678044c2d2099480d
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-02-18 21:12:14 +0000

    Update LoadImages.swift

--- HostelWorld/View/ReusableView/LoadImages.swift
+++ HostelWorld/View/ReusableView/LoadImages.swift
@@ -7,6 +7,23 @@
 
 import SwiftUI
 
+/*
+   The `LoadImages` struct is a SwiftUI view designed for asynchronously loading and displaying images from a given URL.
+
+   - Properties:
+     - width: CGFloat: The desired width of the image.
+     - height: CGFloat: The desired height of the image.
+     - urlString: String: The URL string representing the image location.
+     - isCorner: Bool: Indicates whether to apply a corner radius to the image.
+
+   - Body:
+     - Utilizes the AsyncImage view to handle asynchronous image loading.
+       - Switches over loading phases, including success, failure, and empty states.
+       - Displays the loaded image with specified resizable, corner radius, aspect ratio, and frame properties.
+       - Handles failure and empty states with default images and placeholder views.
+       - Allows customization of corner radius with an option to apply it to specific corners.
+*/
+
 struct LoadImages: View {
     
     
