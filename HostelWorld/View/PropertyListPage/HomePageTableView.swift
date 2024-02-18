873c5236a1db725a5214114785ab902544a53eef
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-02-18 21:09:50 +0000

    Update HomePageTableView.swift

--- HostelWorld/View/PropertyListPage/HomePageTableView.swift
+++ HostelWorld/View/PropertyListPage/HomePageTableView.swift
@@ -7,6 +7,29 @@
 
 import SwiftUI
 
+/*
+   The `HomePageTableView` struct is a SwiftUI view representing the main content view for displaying a list of city properties.
+
+   - Properties:
+     - viewModel: PropertiesViewModel: Manages the data and business logic related to properties.
+     - isRefreshing: Bool: Indicates whether the view is currently being refreshed.
+     - showAlert: Bool: Controls the visibility of an alert for error messages.
+     - showErrorMessage: Bool: Indicates whether to display an error message.
+     - errorMessage: String: The message to be shown in case of an error.
+   
+   - Body:
+     - Utilizes a ZStack to layer various components:
+       - PropertyListScrollView for displaying a scrollable list of city properties.
+       - NavigationDestination for handling navigation to the PropertyDetailScreen when a property is selected.
+       - Redacted state for showing a placeholder UI during loading.
+       - Alert for presenting error messages with retry and OK options.
+       - Pull-to-refresh functionality for reloading the property list.
+       - IndexPage view for an empty property list.
+       - Additional text and styling for cases where there are no properties available.
+     - Triggers the `loadPropertyList` function on load to fetch property data.
+     - Provides a reload button in the navigation bar for manually refreshing the property list.
+*/
+
 struct HomePageTableView: View {
     
     @StateObject private var viewModel =  PropertiesViewModel()
