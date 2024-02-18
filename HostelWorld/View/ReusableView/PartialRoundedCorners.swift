940e44b1dac4bdfa22a56a514cce41ea1e7aa169
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-02-18 21:15:04 +0000

    Update PartialRoundedCorners.swift

--- HostelWorld/View/ReusableView/PartialRoundedCorners.swift
+++ HostelWorld/View/ReusableView/PartialRoundedCorners.swift
@@ -7,6 +7,18 @@
 
 import SwiftUI
 
+/*
+   The `RoundedCorner` struct is a SwiftUI custom shape that generates a path for a rounded rectangle with specific corners.
+
+   - Properties:
+     - radius: CGFloat: The corner radius of the rounded rectangle. Defaults to .infinity.
+     - corners: UIRectCorner: The corners to be rounded, represented by a bitmask. Defaults to .allCorners.
+
+   - Method:
+     - path(in rect: CGRect) -> Path: Generates and returns a Path based on a UIBezierPath created with the specified rounded rectangle parameters.
+*/
+
+
 struct RoundedCorner: Shape {
     var radius: CGFloat = .infinity
     var corners: UIRectCorner = .allCorners
