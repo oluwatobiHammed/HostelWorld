e9e4a8ea2da6582ab655583086765dc9f19460f2
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-02-18 21:11:08 +0000

    Update PropertyListScrollView.swift

--- HostelWorld/View/PropertyListPage/PropertyListScrollView.swift
+++ HostelWorld/View/PropertyListPage/PropertyListScrollView.swift
@@ -7,6 +7,23 @@
 
 import SwiftUI
 
+/*
+   The `PropertyListScrollView` struct is a SwiftUI view responsible for displaying a scrollable list of city properties.
+
+   - Properties:
+     - viewModel: PropertiesViewModel: Manages the data and business logic related to properties.
+     - isLoading: Bool: Indicates whether content is currently loading.
+     - properties: [CityProperty]: An array of city properties to be displayed.
+   
+   - Body:
+     - Utilizes a ScrollView with a LazyVStack for efficiently rendering a vertical stack of city properties.
+     - Iterates over the array of properties and displays each property using the `PropertiesCellView`.
+       - Configures each cell with styling, frame height, and padding for proper layout.
+       - Uses NavigationLink for navigation to the PropertyDetailScreen when a property is selected.
+       - Applies button styles to remove default navigation colors and disables interaction during loading.
+     - Implements redaction for a placeholder UI during loading.
+*/
+
 struct PropertyListScrollView: View {
     
     @StateObject private var viewModel =  PropertiesViewModel()
