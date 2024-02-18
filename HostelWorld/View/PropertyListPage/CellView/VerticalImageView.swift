6bc7d4adee4d3f9491de4169d1ff99383340fd45
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-02-18 21:06:37 +0000

    Update VerticalImageView.swift

--- HostelWorld/View/PropertyListPage/CellView/VerticalImageView.swift
+++ HostelWorld/View/PropertyListPage/CellView/VerticalImageView.swift
@@ -7,9 +7,26 @@
 
 import SwiftUI
 
+/*
+   The `VerticalImageView` struct is a SwiftUI view designed to display a set of vertical images within a TabView.
+
+   - Properties:
+     - images: [PropertyImage]: An array of PropertyImage objects representing the images to be displayed.
+     - currentPage: @State Int: A state variable to keep track of the currently selected page in the TabView.
+   
+   - Body:
+     - Utilizes TabView to create a swipeable container for a set of ScrollView items.
+       - Each ScrollView contains a LazyHStack with LoadImages to display the images horizontally.
+       - The LazyHStack ensures efficient loading and rendering of images.
+     - Implements PageTabViewStyle for visual presentation and navigation of pages.
+     - Resets the currentPage when the app enters the foreground to ensure proper navigation behavior.
+*/
+
 struct VerticalImageView: View {
+    
     let images: [PropertyImage]
     @State private var currentPage: Int = 0
+    
     var body: some View {
         TabView(selection: $currentPage) {
             ForEach(images.indices, id: \.self) { index in
